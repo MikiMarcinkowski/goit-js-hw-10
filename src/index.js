4accbec69c7b0b687d44af17348a015b5de686d8
mikus151 <mikus151@wp.pl>
2023-07-09 23:06:09 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const API_KEY = 'live_hmn1n1og6YXJ06D010VbCePU5aGk8e0cIa7QNNuHApIlzqZTBBCeJjR7zy
 
 // import '../../node_modules/slim-select/dist/slimselect.css';
 
-import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  'live_hmn1n1og6YXJ06D010VbCePU5aGk8e0cIa7QNNuHApIlzqZTBBCeJjR7zyvirFXY';
+// import axios from 'axios';
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_hmn1n1og6YXJ06D010VbCePU5aGk8e0cIa7QNNuHApIlzqZTBBCeJjR7zyvirFXY';
 
   import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js';
 
@@ -17,6 +17,11 @@ const loader = document.querySelector('.loader');
 const errorTheme = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
+
+// import SlimSelect from 'slim-select';
+// new SlimSelect({
+//   select: select,
+// });
 
 
 
