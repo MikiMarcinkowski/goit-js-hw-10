aabf82cc6517ebadce13ee1917c94a62d97e4f99
mikus151 <mikus151@wp.pl>
2023-07-10 23:53:30 +0200

    ad css.file, upgrade cat.api

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,22 +1,11 @@
-const API_KEY =
+
+const url = 'https://api.thecatapi.com/v1/breeds';
+
+const apiKey =
   'live_hmn1n1og6YXJ06D010VbCePU5aGk8e0cIa7QNNuHApIlzqZTBBCeJjR7zyvirFXY';
 
-import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  'live_hmn1n1og6YXJ06D010VbCePU5aGk8e0cIa7QNNuHApIlzqZTBBCeJjR7zyvirFXY';
-
-function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-  });
-}
-
-function fetchCatByBreed(breedId) {
-  return fetch(
-    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
-  ).then(response => {
+export function fetchBreeds() {
+  return fetch(`${url}?api_key=${apiKey}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -24,4 +13,13 @@ function fetchCatByBreed(breedId) {
   });
 }
 
-export { fetchBreeds, fetchCatByBreed };
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?api_key=${apiKey}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
